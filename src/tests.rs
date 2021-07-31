12daa46d53130621476fb916ecf591756af35446
peterhenryd <peterhenryd@gmail.com>
2021-07-31 17:31:48 -0400

    modified test

--- src/tests.rs
+++ src/tests.rs
@@ -1,10 +1,12 @@
 use crate::dorian::Dorian;
-use crate::types::int::IntData;
-use crate::types::function::FnData;
+use crate::llvm::execution_engine::ExtFn;
+use crate::llvm::OptimizationLevel;
 use crate::types::data::TypeData;
-use crate::value::Value;
-use crate::value::int::{Int, BinOp};
+use crate::types::fun::FunData;
+use crate::types::int::IntData;
 use crate::value::data::BuildValue;
+use crate::value::int::{BinOp, Int};
+use crate::value::Value;
 
 #[test]
 fn test() {
@@ -12,23 +14,25 @@ fn test() {
 
     let mut test = dorian.create_module("test");
 
-    let i8 = IntData::new(8)
-        .create(&dorian);
-    let fn_type = FnData::new(vec![&i8, &i8], &i8, false)
-        .create(&dorian);
+    let i64 = IntData::Bits(64).create(&dorian);
+    let fun_type = FunData::new(vec![&i64, &i64], &i64, false).create(&dorian);
 
-    let mut function = test.add_fn("add", &fn_type);
+    let mut fun = test.add_fn("add", &fun_type);
 
-    let mut entry = function.add_block();
-    if let [lhs, rhs] = function.fetch_params().as_slice() {
+    let mut entry = fun.add_block("entry");
+    if let [lhs, rhs] = fun.fetch_params().as_slice() {
         let lhs = lhs.as_int_value().unwrap();
         let rhs = rhs.as_int_value().unwrap();
 
-        let result = Int::Bin(BinOp::Add, &lhs, &rhs)
-            .build(&entry);
+        let result = Int::Bin(BinOp::Add, &lhs, &rhs).build(&entry);
 
         entry.return_value(&result);
     }
 
     println!("{}", test.to_string());
+
+    let engine = test.create_execution_engine(OptimizationLevel::Aggressive);
+    let add = engine.get_fun::<ExtFn<(i64, i64), i64>>("add").unwrap();
+
+    assert_eq!(unsafe { add(5, 4) }, 9);
 }
